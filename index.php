5919c2fdef357ae342c39aa7ff60cb1acfe4d302
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-01-13 00:09:21 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-Learning</title>
+    <style>
+        header {
+            background: #e3e3e3;
+            padding: 2em;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+
+<header>
+    <?php
+
+        $person = $_GET['person'];
+
+        echo "<h1> Hello $person <h1>";
+
+    ?>
+</header>
+    
+</body>
+</html>
