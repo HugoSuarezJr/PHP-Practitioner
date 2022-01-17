488433a6d546b8e3ec641e6d2f63db57c6055535
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-01-17 04:43:35 -0500

    Use Arrays and foreach function

--- index.php
+++ index.php
@@ -1,27 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP-Learning</title>
-    <style>
-        header {
-            background: #e3e3e3;
-            padding: 2em;
-            text-align: center;
-        }
-    </style>
-</head>
-<body>
+<?php
 
-    <h1>
-        <?= "Hi, you are " . htmlspecialchars($_GET['age']); ?> years old
-    </h1>
-    <hr>
-    <h2>
-        <?= "And your favorite hobby is " . htmlspecialchars($_GET['hobby']); ?>!
-    </h2>
-    
-</body>
-</html>
+    $animals = [
+        "Dog",
+        "Koala",
+        "Cow"
+    ]; 
+
+    require "index.view.php";
+
+
+
+?>
