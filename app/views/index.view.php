7b85f8a38b93b758f0db30c6399728807e8cce04
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-02-23 23:47:56 -0500

    Completed App using my own custom MVC framework

--- app/views/index.view.php
+++ app/views/index.view.php
@@ -1,6 +1,7 @@
 <?php require('partials/head.php'); ?>
 
 <h1>Home Page</h1> 
+<h2>This App has been completed with my own custom MVC framework! :)</h2>
 <h4><em>*Select one of the options above !*</em></h4>
 
 
